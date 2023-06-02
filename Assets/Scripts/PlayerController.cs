6aa777d7fa60512335db57131471009fc2f97467
NoLogicTech <80648342+NoLogicTech@users.noreply.github.com>
2023-06-02 13:46:20 +0100

    2.Make the projectile into a prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
     void Start()
     {
         
