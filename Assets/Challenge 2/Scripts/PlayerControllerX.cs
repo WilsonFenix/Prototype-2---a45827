eba805c6365b9c9a72e50b51ae41f58c37e93dd5
NoLogicTech <80648342+NoLogicTech@users.noreply.github.com>
2023-06-02 19:14:27 +0100

    chagenge 2 - 9.Bonus: The player can “spam” the spacebar key

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool onClick = true;
+
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && onClick)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            onClick = false;
+
+            if (!onClick)
+            {
+                StartCoroutine(WaiteForSecond());
+            }
         }
+    }
+    IEnumerator WaiteForSecond()
+    {
+        yield return new WaitForSeconds(1.5f);
+        onClick = true;
     }
 }
